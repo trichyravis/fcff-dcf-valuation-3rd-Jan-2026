4e7231848c1083906eba3ecc54effc3fa6807705
Ravichandran Vaidyanathan <106052503+trichyravis@users.noreply.github.com>
2026-01-03 07:47:05 +0530

    Delete core/monte_carlo.py

--- core/monte_carlo.py
+++
@@ -1,7 +0,0 @@
-
-import numpy as np
-
-def monte_carlo_dcf(fcff, wacc_mean, g_mean, n=5000):
-    wacc = np.random.normal(wacc_mean, 0.01, n)
-    g = np.random.normal(g_mean, 0.005, n)
-    return (fcff * (1 + g)) / (wacc - g)
