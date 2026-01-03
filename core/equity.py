f79ae245872925bf840ef1082b63f18a965402fb
Ravichandran Vaidyanathan <106052503+trichyravis@users.noreply.github.com>
2026-01-03 07:02:24 +0530

    Update equity.py

--- core/equity.py
+++ core/equity.py
@@ -1,25 +1,19 @@
+
 import pandas as pd
 
 def get_share_count(xbrl):
-    """
-    Returns best available share count for valuation.
-    Preference: Diluted → Basic → Outstanding
-    """
-
-    tag_priority = [
+    tags = [
         "WeightedAverageNumberOfDilutedSharesOutstanding",
         "WeightedAverageNumberOfSharesOutstandingBasic",
         "CommonStockSharesOutstanding"
     ]
 
-    for tag in tag_priority:
+    for tag in tags:
         try:
-            data = xbrl["facts"]["us-gaap"][tag]["units"]["shares"]
-            df = pd.DataFrame(data)
+            df = pd.DataFrame(xbrl["facts"]["us-gaap"][tag]["units"]["shares"])
             df = df[df["form"] == "10-K"].sort_values("end")
             return int(df.iloc[-1]["val"])
         except:
             continue
 
-    raise ValueError("Share count not available in 10-K")
-
+    raise ValueError("Share count not available")
