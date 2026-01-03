a5e78dd1df22b55cba016bc02cbe6d0f8b866af1
Ravichandran Vaidyanathan <106052503+trichyravis@users.noreply.github.com>
2026-01-03 05:58:34 +0530

    Update sensitivity.py

--- core/sensitivity.py
+++ core/sensitivity.py
@@ -1,1 +1,12 @@
 
+import pandas as pd
+import numpy as np
+
+def sensitivity_matrix(fcff_last, wacc_range, g_range):
+    matrix = pd.DataFrame(index=wacc_range, columns=g_range)
+
+    for w in wacc_range:
+        for g in g_range:
+            matrix.loc[w, g] = fcff_last*(1+g)/(w-g)
+
+    return matrix
