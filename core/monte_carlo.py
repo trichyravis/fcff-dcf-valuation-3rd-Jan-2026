ba29753c7f5f3eaaa7776c9c6aed4e9459d24866
Ravichandran Vaidyanathan <106052503+trichyravis@users.noreply.github.com>
2026-01-03 07:03:47 +0530

    Update monte_carlo.py

--- core/monte_carlo.py
+++ core/monte_carlo.py
@@ -1,9 +1,7 @@
+
 import numpy as np
 
-def monte_carlo_dcf(fcff_last, wacc_mean, g_mean, sims=10000):
-    wacc = np.random.normal(wacc_mean, 0.01, sims)
-    g = np.random.normal(g_mean, 0.005, sims)
-
-    values = (fcff_last*(1+g))/(wacc-g)
-    return values
-
+def monte_carlo_dcf(fcff, wacc_mean, g_mean, n=5000):
+    wacc = np.random.normal(wacc_mean, 0.01, n)
+    g = np.random.normal(g_mean, 0.005, n)
+    return (fcff * (1 + g)) / (wacc - g)
