c345ef03208d2b8090a2ca5fe8c76c2fd58e166e
Ravichandran Vaidyanathan <106052503+trichyravis@users.noreply.github.com>
2026-01-03 07:47:20 +0530

    Delete core/sensitivity.py

--- core/sensitivity.py
+++
@@ -1,9 +0,0 @@
-
-import pandas as pd
-
-def sensitivity_matrix(fcff, waccs, gs):
-    table = pd.DataFrame(index=waccs, columns=gs)
-    for w in waccs:
-        for g in gs:
-            table.loc[w, g] = (fcff * (1 + g)) / (w - g)
-    return table
