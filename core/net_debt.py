c293417f64f3b28187b3b11bab96f5256dff3c74
Ravichandran Vaidyanathan <106052503+trichyravis@users.noreply.github.com>
2026-01-03 06:32:29 +0530

    Update net_debt.py

--- core/net_debt.py
+++ core/net_debt.py
@@ -1,1 +1,32 @@
+def get_net_debt(xbrl, extract):
+    """
+    Net Debt = Debt – Cash
+    """
+
+    debt_tags = [
+        "LongTermDebt",
+        "LongTermDebtCurrent"
+    ]
+
+    cash_tags = [
+        "CashAndCashEquivalentsAtCarryingValue",
+        "CashCashEquivalentsRestrictedCashAndRestrictedCashEquivalents"
+    ]
+
+    debt = 0
+    cash = 0
+
+    for tag in debt_tags:
+        try:
+            debt += extract(xbrl, [tag], "Debt")["Debt"].iloc[-1]
+        except:
+            pass
+
+    for tag in cash_tags:
+        try:
+            cash += extract(xbrl, [tag], "Cash")["Cash"].iloc[-1]
+        except:
+            pass
+
+    return debt - cash
 
