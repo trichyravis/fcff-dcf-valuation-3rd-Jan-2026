1294d8a36fe6db32c4a9b31314570e4652acade4
Ravichandran Vaidyanathan <106052503+trichyravis@users.noreply.github.com>
2026-01-03 07:03:28 +0530

    Update dcf.py

--- core/dcf.py
+++ core/dcf.py
@@ -1,20 +1,6 @@
 
-import numpy as np
-
-def dcf_equity_value(fcff, wacc, g, net_debt, shares):
-    n = len(fcff)
-    years = np.arange(1, n+1)
-
-    pv_fcff = np.sum(fcff / (1+wacc)**years)
-    tv = fcff[-1]*(1+g)/(wacc-g)
-    pv_tv = tv/(1+wacc)**n
-
-    ev = pv_fcff + pv_tv
+def equity_value_from_fcff(fcff, wacc, g, net_debt, shares):
+    ev = (fcff * (1 + g)) / (wacc - g)
     equity = ev - net_debt
     price = equity / shares
-
-    return {
-        "EnterpriseValue": ev,
-        "EquityValue": equity,
-        "FairPrice": price
-    }
+    return ev, equity, price
